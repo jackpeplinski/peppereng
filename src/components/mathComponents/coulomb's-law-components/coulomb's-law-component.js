d9df5b7a8485662275f8a96e1aa50708b91a1040
jackpeplinski <jackepeplinski@gmail.com>
2020-09-12 18:09:16 -0600

    Data source for common math.

--- src/components/mathComponents/coulomb's-law-components/coulomb's-law-component.js
+++
@@ -1,7 +0,0 @@
-exports.coulombsLaw = {
-  coulombsLawFull:"\\overrightarrow{F} = k \\frac{ |q_1| |q_2| } { r^2 }",
-  coulombsLawFullVector:"\\overrightarrow{F} = k \\frac{ |q_1| |q_2| }{ r^2 }  \\widehat{r}",
-  coulombsLawNoForce:"\frac{ |q_1| |q_2| } { r^2 }",
-};
-
-//why doesn't export const coulombsLawObject work?
